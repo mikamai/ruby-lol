1d1b40d1d4ea7832dff8f821f9ffdb3b34f2b16d
Giovanni Intini <intinig@gmail.com>
2013-12-11 10:44:31 +0100

    More specs. Client class starts to take place

--- lib/lol/client.rb
+++ lib/lol/client.rb
@@ -1,10 +1,15 @@
 module Lol
   class Client
+    attr_accessor :region
+
     # Initializes a Lol::Client
-    # @param [String]
+    # @param api_key [String]
+    # @param options [Hash]
+    # @option options [String] :region ("EUW") The region on which the requests will be made
     # @return [Lol::Client]
-    def initialize api_key
+    def initialize api_key, options = {}
       @api_key = api_key
+      @region = options.delete(:region) || "EUW"
     end
   end
 end
--- spec/lol/client_spec.rb
+++ spec/lol/client_spec.rb
@@ -8,5 +8,25 @@ describe Client do
     it "requires an API key argument" do
       expect { Client.new }.to raise_error(ArgumentError)
     end
+
+    it "accepts a region argument" do
+      expect { Client.new("foo", :region => "NA") }.not_to raise_error
+    end
+
+    it "defaults on EUW as a region" do
+      expect(Client.new("foo").region).to eq("EUW")
+    end
+  end
+
+  describe "#champion" do
+    it "defaults to v1.1" do
+      pending
+    end
+  end
+
+  describe "#champions11" do
+    it "returns all champions" do
+      pending
+    end
   end
 end
