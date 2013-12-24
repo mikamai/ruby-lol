a5d76d328ca9479d53335787774030ce49e9932e
Giovanni Intini <intinig@gmail.com>
2013-12-24 12:52:08 +0100

    Finished implementing API

--- lib/lol/talent.rb
+++ lib/lol/talent.rb
@@ -1,6 +1,20 @@
 module Lol
   class Talent < Model
-    def initialize foo
-    end
+
+    # @!attribute [r] id
+    #   @return [Fixnum] id of talent
+    attr_reader :id
+
+    # @!attribute [r] name
+    #   @return [String] name of talent
+    attr_reader :name
+
+    # @!attribute [r] rank
+    #   @return [Fixnum] rank of talent
+    attr_reader :rank
+
+    private
+
+    attr_writer :id, :name, :rank
   end
 end
