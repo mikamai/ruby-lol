7a98d717a7ee057d715835ff4a2eff3a09df8bf8
andrea longhi <andrea@spaghetticode.it>
2014-01-12 18:58:20 +0100

    add autoloader to manage dependencies

--- lib/lol.rb
+++ lib/lol.rb
@@ -1,32 +1,8 @@
-require "lol/client"
+require 'active_support/core_ext/string/inflections'
+require 'httparty'
+require 'uri'
 
-require "lol/request"
-require "lol/champion_request"
-require "lol/game_request"
-require "lol/stats_request"
-require "lol/team_request"
-require "lol/league_request"
-require "lol/summoner_request"
+module Lol
+end
 
-require "lol/model"
-require "lol/champion"
-require "lol/game"
-require "lol/league"
-require "lol/player"
-require "lol/summoner"
-require "lol/rune_page"
-require "lol/rune_slot"
-require "lol/rune"
-require "lol/mastery_page"
-require "lol/talent"
-require "lol/raw_statistic"
-require "lol/player_statistic"
-require "lol/ranked_statistics_summary"
-require "lol/champion_statistics_summary"
-require "lol/team"
-require "lol/roster"
-require "lol/team_member"
-require "lol/team_statistic"
-require "lol/match_summary"
-require "lol/league_entry"
-require "lol/mini_series"
+require 'lol/autoloader'
