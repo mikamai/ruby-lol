3940e37c3807916d9c5fd1efd74ef0a5425426ba
andrea longhi <andrea@spaghetticode.it>
2014-01-30 15:48:56 +0100

    autoload fix

--- lib/lol/autoloader.rb
+++ lib/lol/autoloader.rb
@@ -1,4 +1,4 @@
-Dir['lib/lol/*.rb'].each do |file|
+Dir["#{File.expand_path('..', __FILE__)}/*.rb"].each do |file|
   filename  = File.basename file
   classname = filename.split('.rb').first.camelize
   Lol.autoload classname, File.expand_path("../#{filename}", __FILE__)
