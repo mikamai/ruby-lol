072f51ff94fd4e0e82d51283f32504e100dcabad
James Fickel <jamesfickel@gmail.com>
2014-03-22 11:47:19 -0700

    added a few extra methods to league request

--- lib/lol/league_request.rb
+++ lib/lol/league_request.rb
@@ -13,5 +13,31 @@ module Lol
       perform_request(api_url("league/by-summoner/#{summoner_id}")).map {|l| League.new l}
     end
 
+    # Retrieves leagues entry data for summoner, including league entries for all of summoner's teams
+    # @param [String]
+    # @return [Array]
+    def get_entries summoner_id
+      perform_request(api_url("league/by-summoner/#{summoner_id}/entry")).map { |e| LeagueEntry.new e }
+    end
+
+    # Retrieves leagues data for team
+    # @param [String]
+    # @return [Array]
+    def by_team team_id
+      perform_request(api_url("league/by-team/#{team_id}")).map { |l| League.new l }
+    end
+
+    # Retrieves leagues entry data for team
+    # @param [String]
+    # @return [Array]
+    def entries_by_team team_id
+      perform_request(api_url("league/by-team/#{team_id}/entry")).map { |e| LeagueEntry.new e }
+    end
+
+    # Retrieves challenger tier leagues
+    # @ return [Array]
+    def challenger
+      perform_request(api_url('league/challenger')).map { |l| League.new l }
+    end
   end
 end
