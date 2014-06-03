21098d78ee8647d0b05e41d8d743583fe0f5ff5b
Nicola Racco <nicola@nicolaracco.com>
2014-06-03 14:55:59 +0200

    store headers in invalidapiresponse exception

--- lib/lol/invalid_api_response.rb
+++ lib/lol/invalid_api_response.rb
@@ -3,8 +3,40 @@ module Lol
     attr_reader :raw
 
     def initialize url, response
-      super "#{response["status"]["message"]} calling #{url}"
-      @raw = response
+      @raw = extract_raw_response_info response
+      super "#{raw[:status]} calling #{url}"
+    end
+
+    private
+
+    def extract_raw_response_info response
+      {
+        headers: extract_raw_headers(response),
+        body:    extract_raw_body(response),
+        status:  extract_raw_status(response)
+      }
+    end
+
+    def extract_raw_headers response
+      response.respond_to?(:headers) && response.headers || {}
+    end
+
+    def extract_raw_body response
+      if response.respond_to?(:parsed_response)
+        response.parsed_response
+      elsif response.respond_to?(:body)
+        response.body
+      else
+        response
+      end
+    end
+
+    def extract_raw_status response
+      if response.is_a?(Hash) && response['status']
+        response['status']['message']
+      else
+        "Unknown Error"
+      end
     end
   end
 end
