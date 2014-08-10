74ca8f0c2aca82bdd505b527bb54b0bac7a9fc19
Cameron Martin <cameronmartin123@gmail.com>
2014-08-10 03:16:32 +0100

    Changed param type signatures.

--- lib/lol/league_request.rb
+++ lib/lol/league_request.rb
@@ -7,7 +7,7 @@ module Lol
     end
 
     # Retrieves leagues data for summoner, including leagues for all of summoner's teams
-    # @param [String]
+    # @param [Array<String>]
     # @return Hash{String => Array<League>}
     def get *summoner_ids
       perform_request(api_url("league/by-summoner/#{summoner_ids.join(",")}")).each_with_object({}) do |(s, l), returns|
@@ -16,7 +16,7 @@ module Lol
     end
 
     # Retrieves leagues entry data for summoner, including league entries for all of summoner's teams
-    # @param [String]
+    # @param [Array<String>]
     # @return [Array]
     # TODO: Change name to entries?
     def get_entries *summoner_ids
@@ -24,14 +24,14 @@ module Lol
     end
 
     # Retrieves leagues data for team
-    # @param [String]
+    # @param [Array<String>]
     # @return [Array]
     def by_team *team_ids
       perform_request(api_url("league/by-team/#{team_ids.join(',')}")).map { |l| League.new l }
     end
 
     # Retrieves leagues entry data for team
-    # @param [String]
+    # @param [Array<String>]
     # @return [Array]
     # TODO: Change name to?
     def entries_by_team *team_ids
