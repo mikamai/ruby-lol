b0c11a274c52481a30cd9c18f8abb9669f32d15e
Giovanni Intini <intinig@gmail.com>
2014-09-03 16:29:32 +0200

    more acceptance specs

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -15,7 +15,8 @@ describe "Live API testing", :remote => true do
 
   # @TODO: Maybe have aliases with singular / plural names so I can do subject.champions?
   let (:champions) { subject.champion.get }
-  let (:intinig) { subject.summoner.by_name("intinig").first }
+  let (:intinig)   { subject.summoner.by_name("intinig").first }
+  let (:team)      { subject.team.by_summoner(intinig.id).first }
 
   describe "champion" do
     it "works on the collection" do
@@ -34,7 +35,17 @@ describe "Live API testing", :remote => true do
   end
 
   describe "league" do
+    it "works with get" do
+      expect {subject.league.get intinig.id}.not_to raise_error
+    end
 
+    it "works with entries" do
+      expect {subject.league.get_entries intinig.id}.not_to raise_error
+    end
+
+    it "works with teams" do
+      expect {subject.league.by_team team.id}.not_to raise_error
+    end
   end
 
   describe "lol-static-data" do
