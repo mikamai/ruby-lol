243cdea3d2a69a001b7a8e0ac8bdaaad4368b209
Giovanni Intini <intinig@gmail.com>
2014-09-03 15:34:10 +0200

    Updated team request

--- lib/lol/team_request.rb
+++ lib/lol/team_request.rb
@@ -8,19 +8,22 @@ module Lol
 
     # Retrieves the list of Teams for the given summoner
     # @return [Array] List of Team
-    def get *summoner_ids
+    def by_summoner *summoner_ids
       returns = {}
       perform_request(api_url "team/by-summoner/#{summoner_ids.join(",")}").each do |s, t|
-        returns[s] = t.map {|data| Team.new data}
+        returns[s] = Team.new t
       end
       returns
     end
 
     # Retrieves the Teams for the given Team ID
     # @return [Team]
-    def getbyid team_id
-      result = perform_request(api_url "team/#{team_id}")
-      Team.new result[team_id]
+    def get *team_ids
+      returns = {}
+      perform_request(api_url "team/#{team_ids.join(",")}").each do |t,d|
+        returns[t] = Team.new d
+      end
+      returns
     end
   end
 end
--- spec/lol/team_request_spec.rb
+++ spec/lol/team_request_spec.rb
@@ -10,10 +10,10 @@ describe TeamRequest do
     expect(TeamRequest.ancestors[1]).to eq(Request)
   end
 
-  describe "#get" do
+  describe "#by_summoner" do
     let(:fixture) { load_fixture('team', TeamRequest.api_version) }
 
-    subject { request.get(1) }
+    subject { request.by_summoner(1) }
 
     before(:each) { stub_request(request, 'team', 'team/by-summoner/1') }
 
@@ -21,32 +21,24 @@ describe TeamRequest do
       expect(subject).to be_a(Hash)
     end
 
-    it 'returns an array of Team' do
-      expect(subject[subject.keys.first].map(&:class).uniq).to eq([Team])
-    end
-
     it 'fetches Team from the API' do
       expect(subject.size).to eq(fixture.size)
     end
   end
 
-  describe "#getbyid" do
+  describe "#get" do
 
-    it 'requires a summoner' do
-      expect { request.getbyid }.to raise_error ArgumentError
+    it 'requires a team id' do
+      expect { request.get }.to raise_error
     end
 
-    context 'with summoner' do
-      subject { request.getbyid("TEAM-a9ad3db0-b377-11e3-b87d-782bcb4ce61a") }
+    context 'with team id' do
+      subject { request.get("TEAM-a9ad3db0-b377-11e3-b87d-782bcb4ce61a") }
 
       before(:each) { stub_request(request, 'team-by-id', 'team/TEAM-a9ad3db0-b377-11e3-b87d-782bcb4ce61a') }
 
-      it 'returns a Team' do
-        expect(subject).to be_a Lol::Team
-      end
-
-      it 'fetches Team from the API' do
-        expect(subject.full_id).to eq "TEAM-a9ad3db0-b377-11e3-b87d-782bcb4ce61a"
+      it 'returns an array of Teams' do
+        subject.each {|k,v| expect(v).to be_a Lol::Team}
       end
     end
 
