e4aa83b01f49aae165e6b295cbbd9424d9b238d7
Nicola Racco <nicola.racco@gmail.com>
2017-05-16 17:48:41 +0200

    updated match request to v3

--- lib/lol/match_request.rb
+++ lib/lol/match_request.rb
@@ -1,31 +1,86 @@
 module Lol
-  class MatchRequest < Request
-    # Returns the supported API Version
-    # @return [String] the supported api version
-    def self.api_version
-      "v2.2"
+  # Bindings for the Match API.
+  #
+  # See: https://developer.riotgames.com/api-methods/#match-v3
+  class MatchRequest < V3Request
+    # @!visibility private
+    def api_base_path
+      "/lol/match/#{self.class.api_version}"
+    end
+
+    # Get match by match ID.
+    # @param [Fixnum] match_id Match ID
+    # @param [String] tournament_code Optional tournament code the match belongs to
+    # @return [DynamicModel] Match representation
+    def find match_id, tournament_code: nil
+      url = "matches/#{match_id}".tap do |u|
+        u << "/by-tournament-code/#{tournament_code}" if tournament_code
+      end
+      DynamicModel.new perform_request api_url url
+    end
+
+    # Get match timeline by match ID.
+    # @param [Fixnum] match_id Match ID
+    # @return [DynamicModel] Timeline represantion
+    def find_timeline match_id
+      DynamicModel.new perform_request api_url "timelines/by-match/#{match_id}"
+    end
+
+    # Get match IDs by tournament code.
+    # @param [String] tournament_code Tournament code
+    # @return [Array<Fixnum>] List of match IDs
+    def ids_by_tournament_code tournament_code
+      perform_request api_url "matches/by-tournament-code/#{tournament_code}/ids"
+    end
+
+    # Get matchlist for ranked games played on given account ID and platform ID and filtered using given filter parameters, if any.
+    # @param [Fixnum] account_id Account ID
+    # @param [Hash] options the options to pass to the call
+    # @option options [Array<Fixnum>] queue Set of queue IDs for which to filtering matchlist.
+    # @option options [Fixnum] beginTime The begin time to use for filtering matchlist specified as epoch milliseconds.
+    # @option options [Fixnum] endTime The end time to use for filtering matchlist specified as epoch milliseconds.
+    # @option options [Fixnum] beginIndex The begin index to use for filtering matchlist.
+    # @option options [Fixnum] endIndex The end index to use for filtering matchlist.
+    # @option options [Array<Fixnum>] season Set of season IDs for which to filtering matchlist.
+    # @option options [Array<Fixnum>] champion Set of champion IDs for which to filtering matchlist.
+    # @return [DynamicModel] MatchList represantion
+    def all options={}, account_id:
+      DynamicModel.new perform_request api_url "matchlists/by-account/#{account_id}", options
+    end
+
+    # Get matchlist for last 20 matches played on given account ID and platform ID.
+    # @param [Fixnum] account_id Account ID
+    # @return [DynamicModel] MatchList represantion
+    def recent account_id:
+      DynamicModel.new perform_request api_url "matchlists/by-account/#{account_id}/recent"
     end
 
     # Returns a match with the given id
+    # @deprecated Please use {MatchRequest#find} instead
     # @param match_id [Fixnum] Match ID
     # @return [Hash] match object
     def get match_id
-      perform_request(api_url("match/#{match_id}"))
+      warn_for_deprecation "MatchRequest#get has been deprecated. Use MatchRequest#get instead"
+      find(match_id).to_h
     end
 
     # Returns a match list for the give tournament code
+    # @deprecated Please use {MatchRequest#ids_by_tournament_code} instead
     # @param tournament_code [String] Tournament code
     # @return [Array] match ids for the given tournament code
     def by_tournament tournament_code
-      perform_request(api_url("match/by-tournament/#{tournament_code}/ids"))
+      warn_for_deprecation "MatchRequest#by_tournament has been deprecated. Use MatchRequest#ids_by_tournament_code instead"
+      ids_by_tournament_code tournament_code
     end
 
     # Returns a match with the given id if is part of the given tournament code
+    # @deprecated Please use {MatchRequest#get} instead
     # @param match_id [Fixnum] Match ID
     # @param tournament_code [String] Tournament code
     # @return [Hash] match object
     def for_tournament match_id, tournament_code
-      perform_request(api_url("match/for-tournament/#{match_id}", { "tournamentCode" => tournament_code }))
+      warn_for_deprecation "MatchRequest#for_tournament(#{match_id}, #{tournament_code}) has been deprecated. Use MatchRequest#find(#{match_id}, tournament_code: #{tournament_code}"
+      find(match_id, tournament_code: tournament_code).to_h
     end
   end
 end
--- spec/lol/match_request_spec.rb
+++ spec/lol/match_request_spec.rb
@@ -4,49 +4,114 @@ require "lol"
 include Lol
 
 describe MatchRequest do
-  it "inherits from Request" do
-    expect(MatchRequest.ancestors[1]).to eq(Request)
+  subject { MatchRequest.new "api_key", "euw" }
+  before { allow(subject).to receive(:warn_for_deprecation) }
+
+  it "inherits from V3Request" do
+    expect(MatchRequest).to be < V3Request
   end
 
-  let(:request) { MatchRequest.new("api_key", "euw") }
+  describe "#find" do
+    context "without a tournament code" do
+      let(:result) { subject.find 1 }
+      before { stub_request subject, 'match', "matches/1" }
 
-  describe "#get" do
-    subject { request.get(1) }
-
-    before { stub_request(request, 'match', 'match/1') }
-
-    it 'returns an hash' do
-      expect(subject).to be_a(Hash)
+      it "returns a DynamicModel" do
+        expect(result).to be_a DynamicModel
+      end
     end
 
-    it 'fetches matches from the API' do
-      fixture = load_fixture('match', MatchRequest.api_version)
-      expect(subject.keys).to match_array fixture.keys
+    context "with a tournament code" do
+      let(:result) { subject.find 1, tournament_code: 2 }
+      before { stub_request subject, 'match-with-tc', "matches/1/by-tournament-code/2" }
+
+      it "returns a DynamicModel" do
+        expect(result).to be_a DynamicModel
+      end
+    end
+  end
+
+  describe "#find_timeline" do
+    it "returns a DynamicModel" do
+      stub_request subject, 'timeline', "timelines/by-match/1"
+      expect(subject.find_timeline 1).to be_a DynamicModel
+    end
+  end
+
+  describe "#ids_by_tournament_code" do
+    it "returns a list of ids" do
+      stub_request subject, 'ids-by-tc', "matches/by-tournament-code/1/ids"
+      result = subject.ids_by_tournament_code '1'
+      expect(result).to be_a Array
+      expect(result.map(&:class).uniq).to eq [Fixnum]
+    end
+  end
+
+  describe "#all" do
+    it "returns a DynamicModel" do
+      stub_request subject, 'matches', "matchlists/by-account/1"
+      expect(subject.all account_id: 1).to be_a DynamicModel
+    end
+  end
+
+  describe "#recent" do
+    it "returns a DynamicModel" do
+      stub_request subject, 'matches-recent', "matchlists/by-account/1/recent"
+      expect(subject.recent account_id: 1).to be_a DynamicModel
+    end
+  end
+
+  describe "#get" do
+    it 'returns an hash' do
+      stub_request subject, 'match', "matches/1"
+      expect(subject.get 1).to be_a Hash
+    end
+
+    it 'calls #find' do
+      expect(subject).to receive(:find).with 1
+      subject.get 1
+    end
+
+    it 'shows a deprecation warning' do
+      stub_request subject, 'match', "matches/1"
+      expect(subject).to receive(:warn_for_deprecation)
+      subject.get 1
     end
   end
 
   describe '#by_tournament' do
-    subject { request.by_tournament 'CODE-FOR-TEST' }
-
-    before { stub_request(request, 'match-by-tournament', 'match/by-tournament/CODE-FOR-TEST/ids') }
-
     it 'returns an Array' do
-      expect(subject).to be_a(Array)
+      stub_request subject, 'ids-by-tc', "matches/by-tournament-code/1/ids"
+      expect(subject.by_tournament 1).to be_a(Array)
+    end
+
+    it 'calls #ids_by_tournament_code' do
+      expect(subject).to receive(:ids_by_tournament_code).with 1
+      subject.by_tournament 1
+    end
+
+    it 'shows a deprecation warning' do
+      stub_request subject, 'ids-by-tc', "matches/by-tournament-code/1/ids"
+      expect(subject).to receive(:warn_for_deprecation)
+      subject.by_tournament 1
     end
   end
 
   describe '#for_tournament' do
-    subject { request.for_tournament 1, 'CODE-FOR-TEST' }
-
-    before { stub_request(request, 'match', 'match/for-tournament/1',{"tournamentCode" => 'CODE-FOR-TEST'}) }
-
     it 'returns an hash' do
-      expect(subject).to be_a(Hash)
+      stub_request subject, 'match-with-tc', "matches/1/by-tournament-code/2"
+      expect(subject.for_tournament 1, '2').to be_a(Hash)
     end
 
-    it 'fetches matches from the API' do
-      fixture = load_fixture('match', MatchRequest.api_version)
-      expect(subject.keys).to match_array fixture.keys
+    it 'calls #find' do
+      expect(subject).to receive(:find).with 1, tournament_code: '2'
+      subject.for_tournament 1, '2'
+    end
+
+    it 'shows a deprecation warning' do
+      stub_request subject, 'match-with-tc', "matches/1/by-tournament-code/2"
+      expect(subject).to receive(:warn_for_deprecation)
+      subject.for_tournament 1, '2'
     end
   end
 end
