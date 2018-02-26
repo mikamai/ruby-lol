61e8cca2f97844d5cee44ecd0febbde55c140999
Kyle Cook <klcook11@gmail.com>
2018-02-25 18:05:57 -0600

    Korean Endpoint Name Incorrect

--- lib/lol/request.rb
+++ lib/lol/request.rb
@@ -40,7 +40,7 @@ module Lol
         :eune => 'eun1',
         :euw  => 'euw1',
         :jp   => 'jp1',
-        :kr   => 'kr1',
+        :kr   => 'kr',
         :lan  => 'la1',
         :las  => 'la2',
         :na   => 'na1',
