e3c591b8981e2aad767f5f08e8704e761298258c
fusillicode <fusillicode@gmail.com>
2019-01-17 21:21:51 +0100

    Update ChampionMastery to V4

--- lib/lol/champion_mastery_request.rb
+++ lib/lol/champion_mastery_request.rb
@@ -1,7 +1,7 @@
 module Lol
   # Bindings for the Champion Mastery API.
   #
-  # See: https://developer.riotgames.com/api-methods/#champion-mastery-v3
+  # See: https://developer.riotgames.com/api-methods/#champion-mastery-v4
   class ChampionMasteryRequest < Request
     # @!visibility private
     def api_base_path
@@ -10,30 +10,30 @@ module Lol
 
     # Get a player's total champion mastery score, which is the sum of individual champion mastery levels
     #
-    # See: https://developer.riotgames.com/api-methods/#champion-mastery-v3/GET_getDynamicModelScore
+    # See: https://developer.riotgames.com/api-methods/#champion-mastery-v4/GET_getDynamicModelScore
     # @param [Integer] summoner_id Summoner ID associated with the player
     # @return [Integer] Player's total champion master score
-    def total_score summoner_id:
-      perform_request api_url "scores/by-summoner/#{summoner_id}"
+    def total_score encrypted_summoner_id:
+      perform_request api_url "scores/by-summoner/#{encrypted_summoner_id}"
     end
 
     # Get all champion mastery entries sorted by number of champion points descending
     #
-    # See: https://developer.riotgames.com/api-methods/#champion-mastery-v3/GET_getAllChampionMasteries
+    # See: https://developer.riotgames.com/api-methods/#champion-mastery-v4/GET_getAllChampionMasteries
     # @param [Integer] summoner_id Summoner ID associated with the player
     # @return [Array<Lol::DynamicModel>] Champion Masteries
-    def all summoner_id:
-      result = perform_request api_url "champion-masteries/by-summoner/#{summoner_id}"
+    def all encrypted_summoner_id:
+      result = perform_request api_url "champion-masteries/by-summoner/#{encrypted_summoner_id}"
       result.map { |c| DynamicModel.new c }
     end
 
     # Get a champion mastery by player ID and champion ID
     #
-    # See: https://developer.riotgames.com/api-methods/#champion-mastery-v3/GET_getDynamicModel
+    # See: https://developer.riotgames.com/api-methods/#champion-mastery-v4/GET_getDynamicModel
     # @param [Integer] summoner_id Summoner ID associated with the player
     # @return [Lol::DynamicModel] Champion Mastery
-    def find champion_id, summoner_id:
-      result = perform_request api_url "champion-masteries/by-summoner/#{summoner_id}/by-champion/#{champion_id}"
+    def find champion_id, encrypted_summoner_id:
+      result = perform_request api_url "champion-masteries/by-summoner/#{encrypted_summoner_id}/by-champion/#{champion_id}"
       DynamicModel.new result
     end
   end
