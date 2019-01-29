6e0944807bdde8c3a0d2f94074738427da93013c
pmcnano <pmc.nano@gmail.com>
2019-01-28 20:10:57 -0600

    Updates SummonerRequest to V4

--- lib/lol/summoner_request.rb
+++ lib/lol/summoner_request.rb
@@ -1,18 +1,18 @@
 module Lol
   # Bindings for the Summoner API.
   #
-  # See: https://developer.riotgames.com/api-methods/#summoner-v3
+  # See: https://developer.riotgames.com/api-methods/#summoner-v4
   class SummonerRequest < Request
     # @!visibility private
     def api_base_path
       "/lol/summoner/#{self.class.api_version}"
     end
 
-    # Get a summoner by summoner ID.
-    # @param [Integer] id Summoner ID
+    # Get a summoner by encrypted summoner ID.
+    # @param [String] encrypted id Summoner ID
     # @return [DynamicModel] Summoner representation
-    def find id
-      DynamicModel.new perform_request api_url "summoners/#{id}"
+    def find encrypted_id
+      DynamicModel.new perform_request api_url "summoners/#{encrypted_id}"
     end
 
     # Get a summoner by summoner name.
@@ -23,11 +23,18 @@ module Lol
       DynamicModel.new perform_request api_url "summoners/by-name/#{name}"
     end
 
-    # Get a summoner by account ID.
-    # @param [Integer] account_id Account ID
+    # Get a summoner by encrypted account ID.
+    # @param [String] account_id encrypted Account ID
     # @return [DynamicModel] Summoner representation
-    def find_by_account_id account_id
-      DynamicModel.new perform_request api_url "summoners/by-account/#{account_id}"
+    def find_by_account_id encrypted_account_id
+      DynamicModel.new perform_request api_url "summoners/by-account/#{encrypted_account_id}"
+    end
+
+    # Get a summoner by encrypted PUUID.
+    # @param [String] account_id encrypted PUUID
+    # @return [DynamicModel] Summoner representation
+    def find_by_puuid encrypted_puuid
+      DynamicModel.new perform_request api_url "summoners/by-puuid/#{encrypted_puuid}"
     end
   end
 end
--- spec/lol/summoner_request_spec.rb
+++ spec/lol/summoner_request_spec.rb
@@ -5,6 +5,7 @@ include Lol
 
 describe SummonerRequest do
   subject { SummonerRequest.new "api_key", "euw" }
+  let(:encrypted_id) { 'qHn0uNkpA1T-NqQ0zHTEqNh1BhH5SAsGWwkZsacbeKBqSdkUEaYOcYNjDomm60vMrLWHu4ulYg1C5Q' }
 
   it "inherits from Request" do
     expect(SummonerRequest).to be < Request
@@ -12,30 +13,44 @@ describe SummonerRequest do
 
   describe "#find" do
     it "returns a DynamicModel" do
-      stub_request subject, "summoner", "summoners/23"
-      expect(subject.find 23).to be_a DynamicModel
+      stub_request subject, "summoner", "summoners/#{encrypted_id}"
+      expect(subject.find encrypted_id).to be_a DynamicModel
     end
   end
 
   describe "#find_by_name" do
     it "returns a DynamicModel" do
-      stub_request subject, 'summoner-by-name', 'summoners/by-name/foo'
+      stub_request subject, 'summoner', 'summoners/by-name/foo'
       expect(subject.find_by_name 'foo').to be_a DynamicModel
     end
 
     it "escapes the given name" do
-      stub_request subject, 'summoner-by-name', 'summoners/by-name/f%C3%B2%C3%A5'
+      stub_request subject, 'summoner', 'summoners/by-name/f%C3%B2%C3%A5'
       subject.find_by_name 'fòå'
     end
 
     it "downcases the given name" do
-      stub_request subject, 'summoner-by-name', 'summoners/by-name/arg'
+      stub_request subject, 'summoner', 'summoners/by-name/arg'
       subject.find_by_name 'ARG'
     end
 
     it 'strips spaces from names' do
-      stub_request(subject, 'summoner-by-name', 'summoners/by-name/foo')
+      stub_request(subject, 'summoner', 'summoners/by-name/foo')
       subject.find_by_name('fo o')
+    end
+  end
+
+  describe "#find_by_account_id" do
+    it "returns a DynamicModel" do
+      stub_request subject, "summoner", "summoners/by-account/#{encrypted_id}"
+      expect(subject.find_by_account_id encrypted_id).to be_a DynamicModel
+    end
+  end
+
+  describe "#find_by_puuid" do
+    it "returns a DynamicModel" do
+      stub_request subject, "summoner", "summoners/by-puuid/#{encrypted_id}"
+      expect(subject.find_by_puuid encrypted_id).to be_a DynamicModel
     end
   end
 end
